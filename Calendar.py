0089b979cb2102498287885fe62cb5b4d26e0ab0
DZburst <115188188+DZburst@users.noreply.github.com>
2023-12-10 23:47:01 +0000

    E1 done, and added remove too ; url format bug fixed.

--- Calendar.py
+++ Calendar.py
@@ -1,8 +1,8 @@
 import sys
 from urllib.parse import quote, unquote
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, url_for
 from markupsafe import escape
-# import time
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -10,18 +10,34 @@ cal = {"Holidays" : ("23/12/2023", "1 382 400", "Everyone")}
 
 @app.route('/')
 def main_menu():
-    return "This is the main page of our project, at the root of the other endpoints. Please choose amongst the following endpoints : \n"
+    current_url = request.headers.get('X-Forwarded-Proto', 'http') + '://' + request.headers.get('X-Forwarded-Host', 'localhost')
+    if current_url.endswith('/'):
+        current_url = current_url[:-1]
+    text = "<h2 style = 'text-align : center;'>This is the main page of our project, at the root of the other endpoints. Please choose amongst the following endpoints. </h2><h2 style = 'text-align : center;'>(Beware when changing the values of T1 and n in the url, keep %25252F for '/' and %252520 for ' ', only change the values)</h2> <br>"
+    
+    # Since the application to implement shouldn't have too many endpoints, we can add manually 
+    # the default values for endpoints which require parameters.
+    # Otherwise, we should create a list and get all the endpoints, access their properties with their rules,
+    # and then do the necessary operations.
 
-@app.route('/viewCalendar', methods = ["GET", "POST"])
+    text += "<p><a href='{}{}'>/{}</a></p><br>".format(current_url, url_for('calendar'), 'calendar')
+    text += "<p><a href='{}{}'>/{}</a></p><br>".format(current_url, url_for('add_event', T1 = quote('01%2F01%2F1970'), t = '86400', p = 'Everyone', n = quote('Day%201')), 'add_event')
+    text += "<p><a href='{}{}'>/{}</a></p><br>".format(current_url, url_for('remove_event', n = 'Day 1'), 'remove_event')
+    return text
+
+@app.route('/viewCalendar', methods = ["GET"])
 def calendar():
     return jsonify(cal)
 
 @app.route('/viewCalendar/addEvent/<T1>/<t>/<p>/<n>', methods = ["GET", "POST"])
 def add_event(T1, t, p, n):
-    
-    cal[n.strip('\"')] = (quote(unquote((T1))).strip('\"'), t.strip('\"'), p.strip('\"'))
+    cal[unquote(unquote(n))] = (unquote(unquote(T1)), t, p)
     return jsonify(cal)
 
+@app.route('/viewCalendar/removeEvent/<n>', methods = ["GET", "POST"])
+def remove_event(n):
+    cal.pop(n)
+    return jsonify(cal)
 
 
 if __name__ == "__main__":
